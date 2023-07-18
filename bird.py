e8588118699ee944994967fcaf96c118e9b5d375
chinamints <xpandasftw@gmail.com>
2023-07-18 11:33:27 -0700

    initial commit

---
+++ bird.py
@@ -0,0 +1,23 @@
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput
+import time
+
+net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = videoSource("/dev/video0")      # '/dev/video0' for V4L2
+display = videoOutput("bird_video.mp4") # 'display://0' 'my_video.mp4' for file
+
+while True:
+    img = camera.Capture()
+
+    if img is None: # capture timeout
+        continue
+
+    detections = net.Detect(img)
+    
+    display.Render(img)
+    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
+    for detection in detections:
+        if net.GetClassDesc(detection.ClassID) == "bird":
+            print("bord")
+            time.sleep(5) # stops spam
